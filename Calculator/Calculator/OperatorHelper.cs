d6f83fce6bf84f5f8d482a443776f3def64dd111
Martin Davidík <martin.davidik@lundegaard.net>
2020-10-10 15:20:56 +0200

    Create class OperatorHelper.cs for proceed calculating operations

---
+++ Calculator/Calculator/OperatorHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public static class OperatorHelper
+    {
+        public static double Calculate(double value1, double value2, string myoperator)
+        {
+            double result = 0;
+            switch (myoperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+
+            }
+
+            return result;
+        }
+    }
+}
